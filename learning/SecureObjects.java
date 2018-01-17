57a30ec3091fd7342dec34790ebde80446166e32
Ankit Katiyar <ankitkatiyar67@gmail.com>
2018-01-17 17:13:59 +0530

    Add files via upload

--- learning/SecureObjects.java
+++ learning/SecureObjects.java
@@ -1,6 +1,7 @@
 package learning;
 
 import java.io.Serializable;
+import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.SecureRandom;
@@ -18,8 +19,7 @@ public class SecureObjects
     {
         try
         {
-            
-            
+
             System.out.println("################### Sealed Object Example ##################");
             Serializable obj = new String("Ankit");
             // Generate key
@@ -28,19 +28,20 @@ public class SecureObjects
             SecretKey aesKey = kgen.generateKey();
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-            
+
             // Sealed Object
             SealedObject sealedObject = new SealedObject(obj, cipher);
             System.out.println("sealedObject-" + sealedObject);
             System.out.println("sealedObject Data-" + sealedObject.getObject(aesKey));
-            
+
             // test signed object
             System.out.println("################### Signed Object Example ##################");
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
             SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
             keyGen.initialize(1024, random);
 
-            PrivateKey signingKey = keyGen.generateKeyPair().getPrivate();
+            KeyPair pair = keyGen.generateKeyPair();
+            PrivateKey signingKey = pair.getPrivate();
 
             Signature signingEngine = Signature.getInstance("DSA");
             signingEngine.initSign(signingKey);
@@ -49,6 +50,10 @@ public class SecureObjects
 
             System.out.println("signedObject-" + signedObject);
             System.out.println("signedObject Data-" + signedObject.getObject());
+            // check authenticity of data
+            boolean bl = signedObject.verify(pair.getPublic(), signingEngine);
+            System.out.println("Authentic: " + bl);
+
         }
 
         catch (Exception e)
