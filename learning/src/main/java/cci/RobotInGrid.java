10178a6ab3ab2b3d759cf8b34dfaa727bb7a79cb
ankitkatiyar91 <ankitkatiyar67@gmail.com>
2021-03-29 01:55:24 +0530

    problem 8.2 find path of robot

--- learning/src/main/java/cci/RobotInGrid.java
+++ learning/src/main/java/cci/RobotInGrid.java
@@ -7,16 +7,18 @@ public class RobotInGrid {
 
     public static void main(String[] args) {
         int[][] grid = new int[][]{
-                {0, 0, 0},
-                {0, 1, 1},
-                {0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0},
         };
-        System.out.println("Sending robot:" + isTherePath(grid));
+        System.out.println("Sending robot:" + isTherePath(grid) + " Path:" + path);
     }
 
     private static boolean isTherePath(int[][] grid) {
         return isTherePathBetween(grid, 0, 0, grid.length - 1, grid[0].length - 1);
     }
+
+    static StringBuilder path = new StringBuilder("(0,0)");
 
     private static boolean isTherePathBetween(int[][] grid, int a, int b, int x, int y) {
 //        System.out.println("Checking (" + a + "," + b + ") and (" + x + "," + y + ")");
@@ -28,13 +30,18 @@ public class RobotInGrid {
         }
         if (x - a <= 1 && y - b <= 1) {
             boolean found = grid[a][b] == 0 && grid[x][y] == 0;
-            System.out.println("Checking (" + a + "," + b + ") and (" + x + "," + y + ") and found: " + found);
+//            System.out.println("Checking (" + a + "," + b + ") and (" + x + "," + y + ") and found: " + found);
+            if (found) {
+                path.append(" -> (" + x + "," + y + ")");
+            } else {
+                path = new StringBuilder("(0,0)");
+            }
             return found;
         }
 
         return ((isTherePathBetween(grid, a, b, x - 1, y - 1) && isTherePathBetween(grid, x - 1, y - 1, x, y))
                 || (isTherePathBetween(grid, a, b, x, y - 1) && isTherePathBetween(grid, x, y - 1, x, y))
-                || (isTherePathBetween(grid, a, b, x - 1, y) && isTherePathBetween(grid, x - 1, y , x, y))
+                || (isTherePathBetween(grid, a, b, x - 1, y) && isTherePathBetween(grid, x - 1, y, x, y))
         );
     }
 }
