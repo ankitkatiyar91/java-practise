b81a7af4955e71e1f75b9ba6e772327a7c1bed23
Ankit Katiyar <ankitkatiyar67@gmail.com>
2021-07-18 18:31:05 +0530

    bubble sort update doc

--- learning/src/main/java/sorting/BubbleSort.java
+++ learning/src/main/java/sorting/BubbleSort.java
@@ -5,11 +5,8 @@ import util.ArrayUtils;
 /**
  * Compare current and it's next element and arrange them in desired order
  *
- * <b>Complexities</b>
- * Worst complexity: n^2
- * Average complexity: n^2
- * Best complexity: n (This is achieved by stopping the algorithm if inner loop did not cause any swap.)
- * Space complexity: 1
+ *
+ * <p><strong>Worst and Average Case Time Complexity: </strong>O(n*n). Worst case occurs when array is reverse sorted.<br><strong>Best Case Time Complexity:</strong> O(n). Best case occurs when array is already sorted.<br><strong>Auxiliary Space:</strong> O(1)<br><strong>Boundary Cases:</strong> Bubble sort takes minimum time (Order of n) when elements are already sorted.<br><strong>Sorting In Place: </strong>Yes<br><strong>Stable:</strong> Yes<br>Due to its simplicity, bubble sort is often used to introduce the concept of a sorting algorithm.&nbsp;<br>In computer graphics it is popular for its capability to detect a very small error (like swap of just two elements) in almost-sorted arrays and fix it with just linear complexity (2n). For example, it is used in a polygon filling algorithm, where bounding lines are sorted by their x coordinate at a specific scan line (a line parallel to x axis) and with incrementing y their order changes (two elements are swapped) only at intersections of two lines (Source: <a target="_blank" rel="noopener noreferrer nofollow" href="http://en.wikipedia.org/wiki/Bubble_sort#In_practice">Wikipedia</a>)<br>&nbsp;&nbsp;<br>&nbsp;</p>
  *
  * @author ankitkatiyar91
  */
